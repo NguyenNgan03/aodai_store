3f81b2dc0b1263826f445a8614c3a69528526ba4
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-01-02 10:27:26 +0700

    update

--- app/views/users/home/homePage.php
+++ app/views/users/home/homePage.php
@@ -1,1 +1,97 @@
-<p> day la trang home</p>
+<body>
+  <div class="main-container">
+    <img class="banner" src="../image/Banner-1.png" />
+    <!-- <div class="slider">
+  
+        <a href="#slide-1">1</a>
+        <a href="#slide-2">2</a>
+        <a href="#slide-3">3</a>
+      
+        <div class="slides">
+          <img id="slide-1" src="../image/Banner-1.png"/>
+          <img id="slide-2" src="../image/Banner-2.png"/>
+          <img id="slide-3" src="../image/Banner-3.png"/>
+        </div>
+      </div> -->
+    <h1 class="san-pham-noi-bat">SẢN PHẨM NỔI BẬT</h1>
+    <div class="flex-row-feb">
+      <img class="imgl-master" src="../image/image1.png" />
+      <img class="img-master" src="../image/image2.png" />
+      <img class="mgm-master" src="../image/image3.png" />
+      <img class="mgl-master" src="../image/image4.png" />
+      <span class="ao-dai-truyen-thong-tinh-khoi-hong">Áo dài Truyền thống Tinh Khôi - Hồng</span>
+      <span class="ao-dai-bach-hoa-xuan-hong-dam">Áo dài Bách Hoa Xuân - Hồng đậm</span>
+      <span class="ao-dai-dang-xua-yen-phuong-hong-pastel">Áo dài Dáng xưa Yên Phương 2024 - Hồng pastel</span>
+      <span class="ao-dai-gam-dang-suon-yen-canh-hong-sen">Áo dài gấm dáng suôn Yên Cảnh - Hồng sen</span>
+      <div class="frame"><span class="text-6">750.000 VNĐ</span></div>
+      <div class="frame-9"><span class="text-7">790.000 VNĐ</span></div>
+      <div class="frame-a"><span class="text-8">750.000 VNĐ</span></div>
+      <div class="frame-b"><span class="text-9">750.000 VNĐ</span></div>
+    </div>
+    <div class="rectangle-copy"></div>
+    <div class="flex-row-ab">
+      <img class="mgl-master-c" src="../image/image5.png" />
+      <img class="mgm-master-d" src="../image/image6.png" />
+      <img class="mgm-master-e" src="../image/image7.png" />
+      <img class="mgl-master-f" src="../image/image8.png" />
+    </div>
+    <div class="flex-row-c">
+      <span class="text-a">Áo dài Dáng xưa Yên Phương 2024 - Đỏ</span><span class="text-b">Áo dài Dáng xưa Yên Phương 2024 - Xanh lục bảo<br /></span><span class="text-c">Áo dài Truyền thống Tinh Khôi - Hồng</span><span class="text-d">Áo dài Truyền thống Tinh Khôi - Hồng</span>
+      <div class="rate-10"></div>
+      <div class="rate-11"></div>
+      <div class="rate-12"></div>
+    </div>
+    <div class="flex-row-bdb">
+      <div class="frame-13"><span class="vnd-14">750.000 VNĐ</span></div>
+      <div class="frame-15"><span class="vnd-16">750.000 VNĐ</span></div>
+      <div class="frame-17"><span class="vnd-18">750.000 VNĐ</span></div>
+      <div class="frame-19"><span class="vnd-1a">750.000 VNĐ</span></div>
+    </div>
+    <span class="xem-tat-ca">Xem tất cả</span>
+    <span class="dich-vu">DỊCH VỤ</span>
+    <div class="flex-row-cd">
+      <img class="refund" src="../image/refund.png" />
+      <img class="support" src="../image/support.png" />
+      <img class="shipping" src="../image/shipping.png" />
+    </div>
+    <div class="flex-row-bf">
+      <span class="mien-phi-van-chuyen">MIỄN PHÍ VẬN CHUYỂN</span><span class="ho-tro-24-7">HỖ TRỢ 24/7</span><span class="hoan-tien-100">HOÀN TIỀN 100%</span>
+    </div>
+    <div class="flex-row-d-1e">
+      <span class="lorem-ipsum-printing">Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.</span><span class="lorem-ipsum-printing-1f">Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.</span><span class="lorem-ipsum-printing-20">Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.</span>
+    </div>
+    <span class="san-pham-ban-chay">SẢN PHẨM BÁN CHẠY</span>
+    <div class="flex-row-ba">
+      <img class="imgl-master-21" src="../image/image1.png" />
+      <img class="img-master-22" src="../image/image2.png" />
+      <img class="mgm-master-23" src="../image/image3.png" />
+      <img class="mgl-master-24" src="../image/image4.png" />
+    </div>
+    <div class="flex-row">
+      <span class="ao-dai-bach-hoa-xuan-hong-dam-25">Áo dài Bách Hoa Xuân - Hồng đậm</span><span class="ao-dai-truyen-thong-tinh-khoi-hong-26">Áo dài Truyền thống Tinh Khôi - Hồng</span><span class="ao-dai-truyen-thong-tinh-khoi-hong-27">Áo dài Truyền thống Tinh Khôi - Hồng</span><span class="ao-dai-truyen-thong">Áo dài Truyền thống Tinh Khôi - Hồng</span>
+    </div>
+    <div class="flex-row-d-28">
+      <div class="rate-29"></div>
+      <div class="rate-2a"></div>
+      <div class="rate-2b"></div>
+      <div class="rate-2c"></div>
+    </div>
+    <div class="flex-row-e">
+      <div class="frame-2d"><span class="text-21">750.000 VNĐ</span></div>
+      <div class="frame-2e"><span class="text-22">750.000 VNĐ</span></div>
+      <div class="frame-2f"><span class="text-23">750.000 VNĐ</span></div>
+      <div class="frame-30"><span class="text-24">750.000 VNĐ</span></div>
+    </div>
+    <div class="base">
+      <span class="text-25">1</span>
+      <span class="text-26">2</span>
+      <span class="text-27">3</span>
+    </div>
+    <div class="banner-trang-chu-vai-may"></div>
+  </div>
+</body>
+
+</html>
