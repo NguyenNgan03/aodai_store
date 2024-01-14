0156696fcaf12764432e1e82c1ed0faa525c8d7a
Van <van.tran25@student.passerellesnumeriques.org>
2024-01-14 19:04:28 +0700

    Render Categories

--- app/views/users/Products/products.php
+++ app/views/users/Products/products.php
@@ -6,18 +6,9 @@
                     <div class="head"><i class="icon fa fa-align-justify fa-fw"></i> Phân loại</div>
                     <nav class="yamm megamenu-horizontal">
                         <ul class="nav">
-                            <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="" aria-hidden="true"></i>Áo dài cách tân</a></li>
-
-                            <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="" aria-hidden="true"></i>Áo dài lụa</a>
-
-                            <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="" aria-hidden="true"></i>Áo dài cưới</a></li>
-
-                            <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class=""></i>ÁO dài truyền thống</a></li>
-
-                            <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class=""></i>Áo dài cho bé</a> </li>
-
-                            <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class=""></i>Áo dài nam</a></li>
-
+                            <?php foreach ($categories as $category) : ?>
+                                <li class="dropdown menu-item"> <a href=<?= "?page=customer&controller=Product&CategoryId=" . $category["id"] ?> class="dropdown-toggle" data-toggle="dropdown"><i class="" aria-hidden="true"></i><?= $category["name"] ?></a>
+                                <?php endforeach ?>
                         </ul>
                     </nav>
                 </div>
@@ -29,40 +20,20 @@
                                 <h4 class="widget-title">Mức giá</h4>
                             </div>
                             <div class="sidebar-widget-body m-t-10">
-                                <div class="price-range-holder"> <span class="min-max"> <span class="pull-left">500.000</span> <span class="pull-right">1.000.000</span> </span>
-                                    <input type="text" id="amount" style="border:0; color:#666666; font-weight:bold;text-align:center;">
-                                    <input type="text" class="price-slider" value="">
-                                </div>
-                                <a href="#" class="lnk btn btn-primary">Show Now</a>
+                                <form action="/?page=customer&controller=Product" method="post">
+                                    <div class="price-range-holder"> <span class="min-max"> <span class="pull-left">500.000</span> <span class="pull-right">2.000.000</span> </span>
+                                        <input type="text" id="amount" style="border:0; color:#666666; font-weight:bold;text-align:center;">
+                                        <input type="text" class="price-slider" name="price" value="">
+                                    </div>
+                                    <button type="submit" class="lnk btn btn-primary">Show Now</button>
+                                </form>
                             </div>
-
                         </div>
-
-
                         <div class="home-banner"> <img src="" alt=""> </div>
                     </div>
                 </div>
             </div>
             <div class='col-md-9'>
-                <div class="clearfix filters-container m-t-10">
-                    <div class="row">
-                        <div class="col col-sm-6 col-md-2">
-                            <div class="filter-tabs">
-                                <!-- <ul id="filter-tabs" class="nav nav-tabs nav-tab-box nav-tab-fa-icon">
-                                    <li class="active"> <a data-toggle="tab" href="#grid-container"><i class="icon fa fa-th-large"></i>Grid</a> </li>
-                                    <li><a data-toggle="tab" href="#list-container"><i class="icon fa fa-th-list"></i>List</a></li>
-                                </ul> -->
-                            </div>
-                        </div>
-
-
-                        <div class="col col-sm-6 col-md-4 text-right">
-                            <div class="pagination-container">
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
                 <div class="search-result-container ">
                     <div id="myTabContent" class="tab-content category-list">
                         <div class="tab-pane active " id="grid-container">
@@ -73,7 +44,7 @@
                                             <div class="products">
                                                 <div class="product">
                                                     <div class="product-image">
-                                                        <div class="image"> <a href="?controller=product&action=detail&page=customer"><img src="<?= $product['Image1']; ?>" alt=""></a> </div>
+                                                        <div class="image"> <a href="?controller=product&action=detail&page=customer"><img src="<?= $product['image1']; ?>" alt=""></a> </div>
                                                         <!-- /.image -->
 
                                                         <div class="tag new"><span>new</span></div>
@@ -91,11 +62,9 @@
                                                         <div class="action">
                                                             <ul class="list-unstyled">
                                                                 <li class="add-cart-button btn-group">
-                                                                    <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
+                                                                    <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> Thêm vào giỏ hàng <i class="fa fa-shopping-cart"></i> </button>
                                                                     <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
                                                                 </li>
-                                                                <li class="lnk wishlist"> <a class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
-                                                                <li class="lnk"> <a class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
                                                             </ul>
                                                         </div>
                                                     </div>
