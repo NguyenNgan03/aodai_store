73b448a423f9814e79bdcd280df8afae38a590f1
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-01-14 21:29:49 +0700

    search function

---
+++ app/models/Search.php
@@ -0,0 +1,25 @@
+<?php
+class Search extends Database {
+    private $db;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function model()
+    {
+        return "search";
+    }
+
+    public function searchProducts($query) {
+        // Using a LIKE query to search for products containing the given name
+        $sql = "SELECT * FROM products WHERE description LIKE :query";
+        $params = ['query' => '%' .$query. '%'];
+        
+        // Execute the query
+        $results = $this->db->query($sql, $params);
+
+        return $results;
+    }
+}
--- app/views/admin/temlate.php
+++ app/views/admin/temlate.php
@@ -11,20 +11,6 @@
     <link rel="stylesheet" href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css' />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <!-- Material Icons -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-  <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
-  <!--SIDEBAR-->
-  <link rel="stylesheet" href="../assets/css/dashboard.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' />
-    <link rel="stylesheet" href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css' />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <?php 
