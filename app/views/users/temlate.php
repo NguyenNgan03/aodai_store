c8a38872b8661103f222b0f829779adfd66b0865
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-01-18 23:29:15 +0700

    a

--- app/views/users/temlate.php
+++ app/views/users/temlate.php
@@ -40,6 +40,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="app\views\public\css\slideshow.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 
