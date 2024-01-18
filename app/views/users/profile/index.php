e859969e27c198dc6b964ada322d9218cf580886
Van <van.tran25@student.passerellesnumeriques.org>
2024-01-18 15:15:51 +0700

    Create interface user profile

---
+++ app/views/users/profile/index.php
@@ -0,0 +1,162 @@
+<link rel="stylesheet" href="app\views\public\css\indexProfile.css">
+<link rel="stylesheet" href="https://allyoucan.cloud/cdn/icofont/1.0.1/icofont.css" integrity="sha384-jbCTJB16Q17718YM9U22iJkhuGbS0Gd2LjaWb4YJEZToOPmnKDjySVa323U+W7Fv" crossorigin="anonymous">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="osahan-account-page-left shadow-sm bg-white h-100">
+                    <div class="border-bottom p-4">
+                        <div class="osahan-user text-center">
+                            <div class="osahan-user-media">
+                                <img class="mb-3 rounded-pill shadow-sm mt-1" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="gurdeep singh osahan">
+                                <div class="osahan-user-media-body">
+                                    <h6 class="mb-2">Thanh Vân</h6>
+                                    <p class="mb-1">555-123-4567</p>
+                                    <p class="mb-1">van.tran@gmail.com</p>
+                                    <a class="text-primary mr-3" href="?controller=home&action=editProfile&page=customer"><i class="icofont-ui-edit"></i> CHỈNH SỬA THÔNG TIN</a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="nav nav-tabs flex-column border-0 pt-4 pl-4 pb-4" id="myTab" role="tablist">
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="osahan-account-page-right shadow-sm bg-white p-4 h-100">
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade  active show" id="favourites" role="tabpanel" aria-labelledby="favourites-tab">
+                            <h4 class="font-weight-bold mt-0 mb-4">Lịch sử đơn hàng</h4>
+                            <div class="row">
+                                <div class="col-md-4 col-sm-6 mb-4 pb-2">
+                                    <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+                                        <div class="list-card-image">
+                                            <div class="favourite-heart text-danger position-absolute"><a href="#"><i class="icofont-heart"></i></a></div>
+                                            <div class="member-plan position-absolute"><span class="badge badge-dark">Đơn hàng</span></div>
+                                            <a href="#">
+                                                <img src="https://product.hstatic.net/1000136076/product/img_5260_158964280db2465392d3fba3beb5a897_master.png" class="img-fluid item-img">
+                                            </a>
+                                        </div>
+                                        <div class="p-3 position-relative">
+                                            <div class="list-card-body">
+                                                <h6 class="mb-1"><a href="#" class="text-black">Áo dài truyền thống Tinh khôi
+                                                    </a>
+                                                </h6>
+                                                <p class="text-gray mb-3">Số lượng: 2</p>
+                                                <p class="text-gray mb-3">Địa chỉ: 99 Tô Hiến Thành, Đà Nẵng</p>
+                                                <p class="text-gray mb-3 time"><span class="float-right text-black-50">1 600 000 VNĐ</span></p>
+                                            </div>
+                                            <div class="list-card-badge">
+                                                <span class="badge badge-danger">Tình trạng: Đã thanh toán</span> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 col-sm-6 mb-4 pb-2">
+                                    <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+                                        <div class="list-card-image">
+                                            <div class="favourite-heart text-danger position-absolute"><a href="#"><i class="icofont-heart"></i></a></div>
+                                            <div class="member-plan position-absolute"><span class="badge badge-dark">Đơn hàng</span></div>
+                                            <a href="#">
+                                                <img src="https://product.hstatic.net/1000136076/product/img_5260_158964280db2465392d3fba3beb5a897_master.png" class="img-fluid item-img">
+                                            </a>
+                                        </div>
+                                        <div class="p-3 position-relative">
+                                            <div class="list-card-body">
+                                                <h6 class="mb-1"><a href="#" class="text-black">Áo dài truyền thống Tinh khôi
+                                                    </a>
+                                                </h6>
+                                                <p class="text-gray mb-3">Số lượng: 2</p>
+                                                <p class="text-gray mb-3">Địa chỉ: 99 Tô Hiến Thành, Đà Nẵng</p>
+                                                <p class="text-gray mb-3 time"><span class="float-right text-black-50">1 600 000 VNĐ</span></p>
+                                            </div>
+                                            <div class="list-card-badge">
+                                                <span class="badge badge-danger">Tình trạng: Đã thanh toán</span> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 col-sm-6 mb-4 pb-2">
+                                    <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+                                        <div class="list-card-image">
+                                            <div class="favourite-heart text-danger position-absolute"><a href="#"><i class="icofont-heart"></i></a></div>
+                                            <div class="member-plan position-absolute"><span class="badge badge-dark">Đơn hàng</span></div>
+                                            <a href="#">
+                                                <img src="https://product.hstatic.net/1000136076/product/img_5260_158964280db2465392d3fba3beb5a897_master.png" class="img-fluid item-img">
+                                            </a>
+                                        </div>
+                                        <div class="p-3 position-relative">
+                                            <div class="list-card-body">
+                                                <h6 class="mb-1"><a href="#" class="text-black">Áo dài truyền thống Tinh khôi
+                                                    </a>
+                                                </h6>
+                                                <p class="text-gray mb-3">Số lượng: 2</p>
+                                                <p class="text-gray mb-3">Địa chỉ: 99 Tô Hiến Thành, Đà Nẵng</p>
+                                                <p class="text-gray mb-3 time"><span class="float-right text-black-50">1 600 000 VNĐ</span></p>
+                                            </div>
+                                            <div class="list-card-badge">
+                                                <span class="badge badge-danger">Tình trạng: Đã thanh toán</span> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 col-sm-6 mb-4 pb-2">
+                                    <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+                                        <div class="list-card-image">
+                                            <div class="favourite-heart text-danger position-absolute"><a href="#"><i class="icofont-heart"></i></a></div>
+                                            <div class="member-plan position-absolute"><span class="badge badge-dark">Đơn hàng</span></div>
+                                            <a href="#">
+                                                <img src="https://product.hstatic.net/1000136076/product/img_5260_158964280db2465392d3fba3beb5a897_master.png" class="img-fluid item-img">
+                                            </a>
+                                        </div>
+                                        <div class="p-3 position-relative">
+                                            <div class="list-card-body">
+                                                <h6 class="mb-1"><a href="#" class="text-black">Áo dài truyền thống Tinh khôi
+                                                    </a>
+                                                </h6>
+                                                <p class="text-gray mb-3">Số lượng: 2</p>
+                                                <p class="text-gray mb-3">Địa chỉ: 99 Tô Hiến Thành, Đà Nẵng</p>
+                                                <p class="text-gray mb-3 time"><span class="float-right text-black-50">1 600 000 VNĐ</span></p>
+                                            </div>
+                                            <div class="list-card-badge">
+                                                <span class="badge badge-danger">Tình trạng: Đã thanh toán</span> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 col-sm-6 mb-4 pb-2">
+                                    <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+                                        <div class="list-card-image">
+                                            <div class="favourite-heart text-danger position-absolute"><a href="#"><i class="icofont-heart"></i></a></div>
+                                            <div class="member-plan position-absolute"><span class="badge badge-dark">Đơn hàng</span></div>
+                                            <a href="#">
+                                                <img src="https://product.hstatic.net/1000136076/product/img_5260_158964280db2465392d3fba3beb5a897_master.png" class="img-fluid item-img">
+                                            </a>
+                                        </div>
+                                        <div class="p-3 position-relative">
+                                            <div class="list-card-body">
+                                                <h6 class="mb-1"><a href="#" class="text-black">Áo dài truyền thống Tinh khôi
+                                                    </a>
+                                                </h6>
+                                                <p class="text-gray mb-3">Số lượng: 2</p>
+                                                <p class="text-gray mb-3">Địa chỉ: 99 Tô Hiến Thành, Đà Nẵng</p>
+                                                <p class="text-gray mb-3 time"><span class="float-right text-black-50">1 600 000 VNĐ</span></p>
+                                            </div>
+                                            <div class="list-card-badge">
+                                                <span class="badge badge-danger">Tình trạng: Đã thanh toán</span> 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
